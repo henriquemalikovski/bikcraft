dc9aee401540456f2b6dbd43cc24985c15bd373f
Henrique Malikovski <hmalikovski@gmail.com>
2020-09-21 22:17:57 -0300

    Inicio do desenvolvimento em Javascript, criação dos sliders da pagina de portfólio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote", // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+  });
+
+  new SimpleSlide({
+    slide: "produtos", // nome do atributo data-slide="principal"
+    nav: true, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+  });
+}
