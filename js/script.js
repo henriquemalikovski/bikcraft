727b28830339cd08ccb3ab13f0ec6a95c8e884b3
Henrique Malikovski <hmalikovski@gmail.com>
2020-09-22 18:42:08 -0300

    Adição da tag de Viewport

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ if (window.SimpleSlide) {
     slide: "produtos", // nome do atributo data-slide="principal"
     nav: true, // se deve ou não mostrar a navegação
     auto: true, // se o slide deve passar automaticamente
-    time: 5000, // tempo de transição dos slides
+    time: 8000, // tempo de transição dos slides
   });
 }
